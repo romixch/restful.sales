211b48635b27b2f35dc75ad4ef2c06bc8bbf5e32
Roman Schaller <roman.schaller@gmail.com>
2014-08-31 16:01:17 +0200

    fix hk2 injection binding

--- src/main/java/ch/romix/restful/sales/SalesBinder.java
+++ src/main/java/ch/romix/restful/sales/SalesBinder.java
@@ -1,12 +1,17 @@
 package ch.romix.restful.sales;
 
+import javax.persistence.EntityManager;
+
 import org.glassfish.hk2.utilities.binding.AbstractBinder;
+
+import ch.romix.restful.sales.logic.SalesService;
 
 public class SalesBinder extends AbstractBinder {
 
   @Override
   protected void configure() {
-    this.bindFactory(SalesEntityManagerFactory.class);
+    this.bindFactory(SalesEntityManagerFactory.class).to(EntityManager.class);
+    this.bind(SalesService.class).to(SalesService.class);
   }
 
 }
