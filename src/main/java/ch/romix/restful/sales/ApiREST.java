a7acf027558dd2da299c49f835859828cfe18fdf
Roman Schaller <roman.schaller@gmail.com>
2014-11-16 23:34:10 +0100

    use URIBuilder

--- src/main/java/ch/romix/restful/sales/ApiREST.java
+++ src/main/java/ch/romix/restful/sales/ApiREST.java
@@ -7,26 +7,37 @@ import java.util.Date;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
+
+import ch.romix.restful.sales.customer.CustomerREST;
+import ch.romix.restful.sales.order.OrderREST;
 
 @Path("/")
 public class ApiREST {
 
   @GET
   @Produces(MediaType.APPLICATION_JSON)
-  public Response dictionary() {
-    return Response.ok(new Dictionary())
-        .expires(Date.from(Instant.now().plus(1, ChronoUnit.HOURS))).build();
+  public Response dictionary(@Context UriInfo uriInfo) {
+    return Response.ok(new Dictionary(uriInfo)).expires(Date.from(Instant.now().plus(1, ChronoUnit.HOURS))).build();
   }
 
   public static final class Dictionary {
+
+    private final UriInfo uriInfo;
+
+    public Dictionary(UriInfo uriInfo) {
+      this.uriInfo = uriInfo;
+    }
+
     public String getOrders() {
-      return "/api/orders";
+      return uriInfo.getBaseUriBuilder().path(OrderREST.class).build().toString();
     }
 
     public String getCustomers() {
-      return "/api/customers";
+      return uriInfo.getBaseUriBuilder().path(CustomerREST.class).build().toString();
     }
   }
 }
